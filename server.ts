883f95862555f6ce28a84feb9578782db6594d3f
Ars8 <pogosov-91@mail.ru>
2021-11-27 13:56:00 +0300

    connect frontend

---
+++ server.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import './core/db';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import { registerValidations } from './validations/register';
+import { passport } from './core/passport';
+
+import { UserCtrl } from './controllers/UserController';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+
+app.get('/auth/verify', registerValidations, UserCtrl.verify);
+app.post('/auth/register', registerValidations, UserCtrl.create);
+app.post('/auth/login', passport.authenticate('local'), UserCtrl.afterLogin);
+
+app.listen(process.env.PORT, (): void => {
+  console.log('SERVER RUNNING!');
+});
