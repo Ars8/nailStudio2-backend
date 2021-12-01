95b89c1c8946e1997420fa9fe65748b1fc0137b0
Ars8 <pogosov-91@mail.ru>
2021-12-01 18:29:31 +0300

    users show

--- server.ts
+++ server.ts
@@ -17,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
+app.get('/users/me', passport.authenticate('jwt', { session: false }), UserCtrl.getUserInfo);
+
 app.get('/auth/verify', registerValidations, UserCtrl.verify);
 app.post('/auth/register', registerValidations, UserCtrl.create);
 app.post('/auth/login', passport.authenticate('local'), UserCtrl.afterLogin);
