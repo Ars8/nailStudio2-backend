8ef451a92c4cdc38f6bf6b51832a96f60485d052
Ars8 <pogosov-91@mail.ru>
2021-12-31 09:57:36 +0300

    hn

--- server.ts
+++ server.ts
@@ -8,8 +8,10 @@ import bodyParser from 'body-parser';
 
 import { registerValidations } from './validations/register';
 import { passport } from './core/passport';
+import { createTweetValidations } from './validations/createTweet';
 
 import { UserCtrl } from './controllers/UserController';
+import { TweetsCtrl } from './controllers/TweetsController';
 
 const app = express();
 
@@ -20,6 +22,13 @@ app.use(passport.initialize());
 app.get('/users', UserCtrl.index);
 app.get('/users/me', passport.authenticate('jwt', { session: false }), UserCtrl.getUserInfo);
 app.get('/users/:id', UserCtrl.show);
+
+app.get('/tweets', TweetsCtrl.index);
+app.get('/tweets/:id', TweetsCtrl.show);
+app.get('/tweets/user/:id', TweetsCtrl.getUserTweets);
+app.delete('/tweets/:id', passport.authenticate('jwt'), TweetsCtrl.delete);
+app.patch('/tweets/:id', passport.authenticate('jwt'), createTweetValidations, TweetsCtrl.update);
+app.post('/tweets', passport.authenticate('jwt'), createTweetValidations, TweetsCtrl.create);
 
 app.get('/auth/verify', registerValidations, UserCtrl.verify);
 app.post('/auth/register', registerValidations, UserCtrl.create);
