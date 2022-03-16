33bbe3b5c7aa1e6a742c4cdfeaf4831f1ea535e9
Arseniy <pogosov-91@mail.ru>
2022-03-16 23:05:17 +0300

    fix problem promise db

--- core/db.ts
+++ core/db.ts
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nailstudi
   useCreateIndex: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-});
+}).catch(console.log);
 
 const db = mongoose.connection;
 
